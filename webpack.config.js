d3519875c8de7d71f1acddfff570b4a951102c6c
itskevm <kevmgaming@gmail.com>
2021-03-07 10:48:19 -0700

    further heroku fixes

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,8 @@ module.exports = {
   mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
+      template: './src/index.html',
+      filename: './index.html'
     })
   ]
 }
