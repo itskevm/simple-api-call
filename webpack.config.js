f1e4636c0b19532581170234943ec64d5da91107
itskevm <kevmgaming@gmail.com>
2021-03-10 12:14:15 -0700

    implemented express server

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'index_bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
   },
   module: {
     rules: [
